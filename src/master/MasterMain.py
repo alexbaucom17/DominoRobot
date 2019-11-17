a979a9e4a0ccede40db67b289d1667996fccc16b
Alex <Baucom>
2019-11-16 17:18:40 -0800

    Putting all the master pieces together

--- src/master/MasterMain.py
+++ src/master/MasterMain.py
@@ -1,25 +1,162 @@
-import Planner
-import config as cfg
+import FieldPlanner
+import config
+import os
+import pickle
+from MarvelMindHandler import RobotPositionHandler
+import time
+from RobotClient import RobotClient
+import msvcrt
+import copy
+import sys
 
-print('Generating domino field from image...',end='',flush=True)
-field = Planner.generateField(cfg)
-print('done.')
 
-print('Generating tiles from field...',end='',flush=True)
-tiles = field.generateTiles()
-print('done.')
+def doWaypointGeneration(cfg, draw = False):
+    print('Generating domino field from image...',end='',flush=True)
+    field = FieldPlanner.generateField(cfg)
+    print('done.')
 
-print('Generating robot paths...',end='',flush=True)
-waypoints = Planner.generateWaypoints(tiles, cfg)
-print('done.')
+    print('Generating tiles from field...',end='',flush=True)
+    tiles = field.generateTiles()
+    print('done.')
 
-print('Generating tile build instructions...',end='',flush=True)
-#build_instructions = tiles.generateBuildInstructions()
-print('done.')
+    print('Generating robot paths...',end='',flush=True)
+    waypoints = FieldPlanner.generateWaypoints(tiles, cfg)
+    print('done.')
 
-print('Generating output diagrams:')
-#field.printStats()
-#field.show()
-#tiles.draw()
-#tiles.show_ordering()
-waypoints.drawWaypoints(40)
+    print('Generating tile build instructions...',end='',flush=True)
+    #build_instructions = tiles.generateBuildInstructions()
+    print('done.')
+
+    if draw:
+        print('Generating output diagrams:')
+        #field.printStats()
+        #field.show()
+        #tiles.draw()
+        #tiles.show_ordering()
+        waypoints.drawWaypoints(40)
+
+    return waypoints
+
+class Rate:
+
+    def __init__(self, rate):
+        """
+        Smart sleeping to maintain rate (Hz)
+        """
+        self.sleep_time = 1.0/rate
+        self.prev_time = None
+
+    def sleep(self):
+        if self.prev_time:
+            dt = time.monotonic() - self.prev_time
+            des_sleep_time = self.sleep_time - dt
+            if des_sleep_time > 0:
+                time.sleep(des_sleep_time)
+        
+        self.prev_time = time.monotonic()
+
+
+class InputHandler:
+
+    def __init__(self):
+        self.in_str = ""
+        self.ready = False
+
+    def service(self):
+        if msvcrt.kbhit():
+            new_key = msvcrt.getche()
+            # print("Got: {}".format(new_key))
+            if new_key == b'\r':
+                self.ready = True                
+            else:
+                self.in_str += new_key.decode('UTF-8')
+
+    def get_data(self):
+        if self.ready:
+            out_str = copy.copy(self.in_str)
+            self.in_str = ""
+            self.ready = False
+        else:
+            out_str = ""
+        return out_str
+
+    
+
+class Master:
+
+    def __init__(self, cfg):
+
+        print("Initializing Master")
+
+        # Handle initial generation or loading of waypoints
+        if os.path.exists(cfg.plan_file):
+            with open(cfg.plan_file, 'rb') as f:
+                self.waypoints = pickle.load(f)
+                print("Loaded waypoint data from {}".format(cfg.plan_file))
+        else:
+            self.waypoints = doWaypointGeneration(cfg)
+            with open(cfg.plan_file, 'wb') as f:
+                pickle.dump(self.waypoints, f)
+                print("Saved waypoint data to {}".format(cfg.plan_file))
+
+        # robot ID - TODO handle multiple
+        robot_id = 1
+
+        # Setup marvelmind devices and position handler
+        self.pos_handler = RobotPositionHandler()
+        self.pos_handler.wake_robot(robot_id)
+
+        # Setup robot clients and communication
+        self.robot_client = RobotClient(robot_id)
+
+        # Setup other miscellaneous variables
+        self.rate = Rate(20)
+        self.input_handler = InputHandler()
+        self.cfg = cfg
+
+        # Wait a little while for setup to finish
+        sleep_time = 30
+        print('Waiting {} seconds for beacons to fully wake up'.format(sleep_time))
+        time.sleep(sleep_time)
+
+        print("Starting loop")
+
+    def cleanup(self):
+
+        print("Cleaning up")
+        self.pos_handler.sleep_robot(1)
+        self.pos_handler.close()
+
+    def check_input(self):
+
+        self.input_handler.service()
+        data = self.input_handler.get_data()
+        if data:
+            if "move" in data:
+                start_idx = data.find('[')
+                end_idx = data.find(']')
+                vals = data[start_idx+1:end_idx].split(',')
+                vals = [x.strip() for x in vals]
+                print("Got move command with parameters [{},{},{}]".format(vals[0], vals[1], vals[2]))
+                self.robot_client.move(float(vals[0]), float(vals[1]), float(vals[2]))
+            elif "quit" in data:
+                self.cleanup()
+                sys.exit()
+
+
+    def loop(self):
+
+        while True:
+            self.check_input()
+            #self.pos_handler.service_queues()
+            self.rate.sleep()
+
+
+
+
+
+if __name__ == '__main__':
+    cfg = config.Config()
+    m = Master(cfg)
+    m.loop()
+
--- src/master/config.py
+++ src/master/config.py
@@ -1,56 +1,63 @@
 import numpy as np
+import os 
 
-# Image configuration
-image_name = 'MR.jpg'
-desired_width = 200
-desired_height = 200
-dominoes = np.array(
-            [('black', (0,0,0)),
-             ('red',   (1,0,0)),
-             ('blue',  (0,0,1)),
-             ('green', (0,1,0)),
-             ('white', (1,1,1))
-             ])
+class Config:
 
-# To get image to look right, need spacing to create square pixels for now
-# could fix this later by making image field creation take into account non-square pixels
-domino_width  = 0.024 # meters
-domino_height = 0.008 # meters
-domino_spacing_x = 0.008 # meters
-domino_spacing_y = 0.024 # meters
+    config_dir_path = os.path.dirname(os.path.realpath(__file__))
+    plan_file = os.path.join(config_dir_path, 'plan.p')
 
-# Spacing for drawing dominoes as pixels instead of rectangles
-meters_per_pixel = 0.008
-domino_width_px = round(domino_width / meters_per_pixel)
-domino_height_px = round(domino_height / meters_per_pixel)
-domino_spacing_x_px = round(domino_spacing_x / meters_per_pixel)
-domino_spacing_y_px = round(domino_spacing_y / meters_per_pixel)
 
-# Tile configuration
-tile_width = 20
-tile_height = 20
-tile_background_color = (0.8, 0.8, 0.8)
-tile_edge_color = (0,0,1)
-tile_size_x_meters = tile_width * (domino_spacing_x + domino_width)
-tile_size_y_meters = tile_height * (domino_spacing_y + domino_height)
+    # Image configuration
+    image_name = os.path.join(config_dir_path, 'MR.jpg')
+    desired_width = 200
+    desired_height = 200
+    dominoes = np.array(
+                [('black', (0,0,0)),
+                ('red',   (1,0,0)),
+                ('blue',  (0,0,1)),
+                ('green', (0,1,0)),
+                ('white', (1,1,1))
+                ])
 
-# Map configuration (distances in meters, angles in degrees)
-robot_boundaries = np.array([[0,0],[20,15]])
-base_station_boundaries = np.array([[3,0],[10,2]])
-charge_station_boundaries = np.array([[3,10],[4,13]])
-tile_drop_location = np.array([3,2,-90]) # X, Y, theta
-tile_pickup_location = np.array([9,2,-90]) # X, Y, theta
-domino_field_origin = np.array([12,6])
-field_width = tile_size_x_meters * desired_width/tile_width
-field_height = tile_size_y_meters * desired_height/tile_height
-domino_field_boundaries = np.array([domino_field_origin,domino_field_origin + np.array([field_width,field_height])])
-prep_position_distance = 2 # How far out of field boundaries to do robot prep move
-exit_position_distance = 2 # How far out of the field boundaries to move to exit
+    # To get image to look right, need spacing to create square pixels for now
+    # could fix this later by making image field creation take into account non-square pixels
+    domino_width  = 0.024 # meters
+    domino_height = 0.008 # meters
+    domino_spacing_x = 0.008 # meters
+    domino_spacing_y = 0.024 # meters
 
-# Arm configuration
-n_arms = 3
+    # Spacing for drawing dominoes as pixels instead of rectangles
+    meters_per_pixel = 0.008
+    domino_width_px = round(domino_width / meters_per_pixel)
+    domino_height_px = round(domino_height / meters_per_pixel)
+    domino_spacing_x_px = round(domino_spacing_x / meters_per_pixel)
+    domino_spacing_y_px = round(domino_spacing_y / meters_per_pixel)
 
-# Robot configuration
-n_available_robots = 4
-n_charging_ports = 2
-frame_tile_to_robot = np.array([0.2, -0.25])
+    # Tile configuration
+    tile_width = 20
+    tile_height = 20
+    tile_background_color = (0.8, 0.8, 0.8)
+    tile_edge_color = (0,0,1)
+    tile_size_x_meters = tile_width * (domino_spacing_x + domino_width)
+    tile_size_y_meters = tile_height * (domino_spacing_y + domino_height)
+
+    # Map configuration (distances in meters, angles in degrees)
+    robot_boundaries = np.array([[0,0],[20,15]])
+    base_station_boundaries = np.array([[3,0],[10,2]])
+    charge_station_boundaries = np.array([[3,10],[4,13]])
+    tile_drop_location = np.array([3,2,-90]) # X, Y, theta
+    tile_pickup_location = np.array([9,2,-90]) # X, Y, theta
+    domino_field_origin = np.array([12,6])
+    field_width = tile_size_x_meters * desired_width/tile_width
+    field_height = tile_size_y_meters * desired_height/tile_height
+    domino_field_boundaries = np.array([domino_field_origin,domino_field_origin + np.array([field_width,field_height])])
+    prep_position_distance = 2 # How far out of field boundaries to do robot prep move
+    exit_position_distance = 2 # How far out of the field boundaries to move to exit
+
+    # Arm configuration
+    n_arms = 3
+
+    # Robot configuration
+    n_available_robots = 4
+    n_charging_ports = 2
+    frame_tile_to_robot = np.array([0.2, -0.25])
