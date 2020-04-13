09cabe13d7bc149b1d0a2d092451028e2da4f7f8
Alex <Baucom>
2020-04-13 13:43:45 -0700

    Nano sketch works well

--- src/stepper_driver/stepper_driver.ino
+++ src/stepper_driver/stepper_driver.ino
@@ -1,10 +1,10 @@
 // Uses stepper driver library from here: https://github.com/DIMRobotics/ArduinoStepperDriver
 #include <StepperDriver.h>
 
-#define STEP_PIN 1
-#define DIR_PIN 0   // Note that the DIR pin is actually controlled by the mster arduino so this won't be connected to anything
+#define STEP_PIN 5
+#define DIR_PIN 1   // Note that the DIR pin is actually controlled by the mster arduino so this won't be connected to anything
 #define STEPS_PER_REV 1024 // How many steps the motor takes per revolution
-#define VEL_PIN 10
+#define VEL_PIN 19
 
 
 // This is assuming a mapping of 0V -> 5V = 0 steps/sec -> max_vel steps/sec
@@ -14,18 +14,27 @@ const float voltage_to_speed = static_cast<float>(max_vel) / static_cast<float>(
 
 axis_t motor;
 int voltage;
-int32_t speed;
+int32_t vel;
+//unsigned long prevMillis;
 
 void setup() 
 {
+//    Serial.begin(115200);
     StepperDriver.init();
     motor = StepperDriver.newAxis(STEP_PIN, DIR_PIN, STEPS_PER_REV);
+//    prevMillis = millis();
 }
 
 void loop()
 {
+//    Serial.print(millis() - prevMillis);
+//    prevMillis = millis();
     voltage = analogRead(VEL_PIN);
-    speed = static_cast<int32_t>(voltage_to_speed * voltage);
-    StepperDriver.write(motor, speed);
+    vel = static_cast<int32_t>(voltage_to_speed * voltage);
+//    Serial.print(", ");
+//    Serial.print(voltage);
+//    Serial.print(", ");
+//    Serial.println(vel);
+    StepperDriver.write(motor, vel);
     delay(10); // Run loop to update velocity at 100 Hz
 }
