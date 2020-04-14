1909ee5ca02fa62fc44cb8e1ed61345aebaeaed7
Alex <Baucom>
2020-04-14 13:53:53 -0700

    Stepper driver tested and I think working

--- src/stepper_driver/stepper_driver.ino
+++ src/stepper_driver/stepper_driver.ino
@@ -1,40 +1,63 @@
-// Uses stepper driver library from here: https://github.com/DIMRobotics/ArduinoStepperDriver
-#include <StepperDriver.h>
+// Uses stepper driver library from here: https://www.airspayce.com/mikem/arduino/AccelStepper/classAccelStepper.html
+#include <AccelStepper.h>
 
-#define STEP_PIN 5
+#define STEP_PIN 2
 #define DIR_PIN 1   // Note that the DIR pin is actually controlled by the mster arduino so this won't be connected to anything
 #define STEPS_PER_REV 1024 // How many steps the motor takes per revolution
-#define VEL_PIN 19
+#define VEL_PIN A0
+
+#define MILLIS_BETWEEN_POLLING 10
+#define MILLIS_BETWEEN_PRINTING 500
 
 
 // This is assuming a mapping of 0V -> 5V = 0 steps/sec -> max_vel steps/sec
-const int max_vel = 1024; //steps/second
+const int max_vel = 100; //steps/second
 const int analog_resolution = 1024; // how many discrete steps available in analogRead
 const float voltage_to_speed = static_cast<float>(max_vel) / static_cast<float>(analog_resolution);
 
-axis_t motor;
+AccelStepper motor;
 int voltage;
-int32_t vel;
-unsigned long prevMillis;
+float vel;
+unsigned long count = 0;
+unsigned long prevMillisRead;
+unsigned long prevMillisPrint;
 
 void setup() 
 {
-//    Serial.begin(115200);
-    StepperDriver.init();
-    motor = StepperDriver.newAxis(STEP_PIN, DIR_PIN, STEPS_PER_REV);
-//    prevMillis = millis();
+    Serial.begin(115200);
+    motor = AccelStepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN);
+    motor.setMaxSpeed(max_vel);
+    motor.setAcceleration(max_vel); // Don't cap acceleration here, we handle accel in the master code
+    motor.setMinPulseWidth(10); // Min from docs is 2.5 microseconds
+    prevMillisRead = millis();
+    prevMillisPrint = millis();
 }
 
 void loop()
 {
-//    Serial.print(millis() - prevMillis);
-//    prevMillis = millis();
-    voltage = analogRead(VEL_PIN);
-    vel = static_cast<int32_t>(voltage_to_speed * voltage);
-//    Serial.print(", ");
-//    Serial.print(voltage);
-//    Serial.print(", ");
-//    Serial.println(vel);
-    StepperDriver.write(motor, vel);
-    delay(10); // Run loop to update velocity at 100 Hz
+
+    if(millis() - prevMillisRead > MILLIS_BETWEEN_POLLING)
+    {
+      voltage = analogRead(VEL_PIN);
+      vel = voltage_to_speed * voltage;
+      motor.setSpeed(vel);
+      prevMillisRead = millis();
+
+      if(millis() - prevMillisPrint > MILLIS_BETWEEN_PRINTING)
+      {
+       
+       Serial.print(count);
+       Serial.print(", ");
+       Serial.print(voltage);
+       Serial.print(", ");
+       Serial.println(vel); 
+       prevMillisPrint = millis();
+       count = 0;
+      }
+      
+    }
+    
+    motor.runSpeed();
+    count++;
+    
 }
