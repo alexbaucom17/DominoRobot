1c0a08b80c30a574bfe3123b3a1f4bce9f5a3837
Alex <alexbaucom17@gmail.com>
2020-06-28 05:23:27 +0100

    First test

---
+++ raspi_test/src/hello.cpp
@@ -0,0 +1,7 @@
+#include "hello.h"
+#include <stdio.h>
+
+void printHello()
+{
+    printf("Hello world\n");
+}
---
+++ raspi_test/src/helloworld.cpp
@@ -0,0 +1,8 @@
+#include "hello.h"
+
+int main()
+{
+    printHello();
+
+    return(0);
+}
