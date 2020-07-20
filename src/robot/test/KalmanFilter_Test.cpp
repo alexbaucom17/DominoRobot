dc8f9ad72da32fee92f05482ae3b0462e5d70b35
Alex <alexbaucom17@gmail.com>
2020-07-20 13:14:22 -0700

    Can run simple unit tests

--- src/robot/test/KalmanFilter_Test.cpp
+++ src/robot/test/KalmanFilter_Test.cpp
@@ -1,3 +1,17 @@
-#include "KalmanFilter.h"
-#include <Eigen/Dense>
+// #include "KalmanFilter.h"
+// #include <Eigen/Dense>
+
+#include <Catch/catch.hpp>
+
+unsigned int Factorial( unsigned int number ) {
+  return number > 1 ? Factorial(number-1)*number : 1;
+}
+
+TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    REQUIRE( Factorial(0) == 1 );
+    REQUIRE( Factorial(1) == 1 );
+    REQUIRE( Factorial(2) == 2 );
+    REQUIRE( Factorial(3) == 6 );
+    REQUIRE( Factorial(10) == 3628800 );
+}
 
