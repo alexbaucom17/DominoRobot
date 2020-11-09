ea172da832a3f2c6a0399f935d58cdcc99eca28d
alex <alexbaucom17@gmail.com>
2020-11-09 07:58:26 -0800

    Starting work on abstracting system time

--- src/robot/src/utils.h
+++ src/robot/src/utils.h
@@ -4,6 +4,8 @@
 #include <chrono>
 #include <vector>
 
+using ClockTimePoint = std::chrono::time_point<std::chrono::steady_clock>;
+
 
 // Utility for getting sign of values
 // From: https://stackoverflow.com/questions/1903954/is-there-a-standard-sign-function-signum-sgn-in-c-c
@@ -44,6 +46,75 @@ class TimeRunningAverage
     bool filled_;
     bool started_;
     std::chrono::time_point<std::chrono::steady_clock> prev_time_;
+
+};
+
+class Timer
+{
+  public:
+    Timer();
+    void reset();
+    int dt_us();
+    int dt_ms();
+    float dt_s();
+
+  private:
+    ClockTimePoint prev_time_;
+    ClockWrapperBase* clock_;
+};
+
+
+ClockTimePoint get_time()
+{
+  return ClockFactory::getFactoryInstance()->get_clock()->now();
+}
+
+class ClockWrapperBase
+{
+ public:
+  virtual ClockTimePoint now() = 0;
+};
+
+class ClockWrapper : public ClockWrapperBase
+{
+ public:
+  ClockTimePoint now() override;
+};
+
+class MockClockWrapper : public ClockWrapperBase
+{
+ public:
+  ClockTimePoint now() override;
+  void advance_sec(float sec);
+  void advance_ms(int ms);
+  void advance_us(int us);
+ private:
+  ClockTimePoint internal_time_;
+};
+
+class ClockFactory
+{
+ public:
+  enum class Mode
+  {
+    NORMAL,
+    MOCK
+  };
+
+  static ClockFactory* getFactoryInstance();
+  void set_mode(ClockFactory::Mode mode);
+  ClockWrapperBase* get_clock();
+
+  // Delete copy and assignment constructors
+  ClockFactory(ClockFactory const&) = delete;
+  ClockFactory& operator= (ClockFactory const&) = delete;
+ 
+ private:
+  // Make standard constructor private so it can't be created
+  ClockFactory();
+  static ClockFactory* factory_instance_;
+  std::unique_ptr<ClockWrapperBase> clock_instance_;
+  ClockFactory::Mode mode_;
 
 };
 
