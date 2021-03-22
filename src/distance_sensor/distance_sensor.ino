71de3eac654aa50c636bb59e46284bc1caaf0bdf
Alex <alexbaucom17@gmail.com>
2021-03-22 07:16:14 -0700

    Update distance sketch to handle 4 sensors

--- src/distance_sensor/distance_sensor.ino
+++ src/distance_sensor/distance_sensor.ino
@@ -1,8 +1,14 @@
 #include "SerialComms.h"
 
-#define NUM_SENSORS 1
+#define NUM_SENSORS 4
 #define TRIGGER_PIN_1 6
-#define ECHO_PIN_1 7 
+#define TRIGGER_PIN_2 7
+#define TRIGGER_PIN_3 8
+#define TRIGGER_PIN_4 9
+#define ECHO_PIN_1 10 
+#define ECHO_PIN_2 11 
+#define ECHO_PIN_3 12 
+#define ECHO_PIN_4 13 
 #define LED_PIN LED_BUILTIN
 
 #define MICROSECONDS_TO_MILLIMETERS 0.343
@@ -11,8 +17,8 @@
 
 SerialComms comm(Serial);
 int distances[NUM_SENSORS];
-int trigger_pins[] = {TRIGGER_PIN_1};
-int echo_pins[] = {ECHO_PIN_1};
+int trigger_pins[] = {TRIGGER_PIN_1,TRIGGER_PIN_2,TRIGGER_PIN_3,TRIGGER_PIN_4};
+int echo_pins[] = {ECHO_PIN_1,ECHO_PIN_2,ECHO_PIN_3,ECHO_PIN_4};
 bool running = false;
 unsigned long start_time = millis();
 
